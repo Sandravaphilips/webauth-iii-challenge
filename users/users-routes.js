9eeff7070480a5211f9d6d1bf1c5a1341e14cdd0
Sandrava Philips <lacasandra1@gmail.com>
2019-11-13 15:35:35 +0100

    MVP and one stretch task completed

--- users/users-routes.js
+++ users/users-routes.js
@@ -18,11 +18,11 @@ router.post('/register', (req, res) => {
         res.status(500).json({message: err}))
 })
 
-router('/login', (req, res) => {
+router.post('/login', (req, res) => {
     const {username, password} = req.body;
 
     Users.findBy({username})
-    .then(user => {
+    .then(([user]) => {
         if(user && bcrypt.compareSync(password, user.password)) {
             const token = generateToken(user);
             res.status(200).json({message: `Hello ${user.username}! Welcome to your dashboard`, payload: token});
@@ -40,13 +40,13 @@ function generateToken(user) {
       department: user.department
     }
     const options = {
-      expiresIn: '1h',
+      expiresIn: '1h'
     }
   
     const result = jwt.sign(
       payload,
       'THIS IS THE SECRET',
-      options,
+      options
     )
   
     return result;
